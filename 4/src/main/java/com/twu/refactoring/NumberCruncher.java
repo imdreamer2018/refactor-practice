81401c2e7d38339a153f1186cc4a815e38c8faa1
imdreamer <743295483@qq.com>
2020-09-15 10:09:37 +0800

    [yangqian] fix: refactor Number Cruncher by Predicate.

--- 4/src/main/java/com/twu/refactoring/NumberCruncher.java
+++ 4/src/main/java/com/twu/refactoring/NumberCruncher.java
@@ -1,5 +1,7 @@
 package com.twu.refactoring;
 
+import java.util.function.Predicate;
+
 public class NumberCruncher {
     private final int[] numbers;
 
@@ -8,33 +10,26 @@ public class NumberCruncher {
     }
 
     public int countEven() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) count++;
-        }
-        return count;
+        return count(o -> o % 2 == 0);
     }
 
+
     public int countOdd() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 1) count++;
-        }
-        return count;
+        return count(o -> o % 2 == 1);
     }
 
     public int countPositive() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number >= 0) count++;
-        }
-        return count;
+        return count(o -> o >= 0);
     }
 
     public int countNegative() {
+        return count(o -> o < 0);
+    }
+
+    private int count(Predicate<Integer> o) {
         int count = 0;
         for (int number : numbers) {
-            if (number < 0) count++;
+            if (o.test(number)) count++;
         }
         return count;
     }
